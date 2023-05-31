18827f0d4e6df3adfff0e685c27716d945d294f7
Adema <aatygaeva08@gmail.com>
2023-06-01 00:49:57 +0600

    1. кол-во ненулевых элементов вручную введенного массива 2.пересечение прямых

---
+++ task 41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+Console.WriteLine("Введите элементы массива: ");
+for(int i = 0; i < size; i++) {
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(string.Join(", ", array));
+int count = 0;
+for(int i = 0; i < size; i++) {
+    if(array[i] > 0) {
+        count++;
+    }
+}
+Console.WriteLine($"{count}");
